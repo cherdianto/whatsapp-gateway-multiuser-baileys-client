c48b75090c76c50057bcb15010f338333c468416
cherdianto <candraherdianto90@gmail.com>
2023-02-11 15:52:48 +0100

    initial commit

--- src/Components/Common/ProfileDropdown.js
+++ src/Components/Common/ProfileDropdown.js
@@ -15,7 +15,7 @@ const ProfileDropdown = () => {
     //     userName : 'candra herdianto'
     // }
 
-    const [userName, setUserName] = useState("Admin");
+    // const [userName, setUserName] = useState("Admin");
 
     // useEffect(() => {
     //     if (sessionStorage.getItem("authUser")) {
@@ -37,8 +37,8 @@ const ProfileDropdown = () => {
                         <img className="rounded-circle header-profile-user" src={avatar1}
                             alt="Header Avatar" />
                         <span className="text-start ms-xl-2">
-                            <span className="d-none d-xl-inline-block ms-1 fw-medium user-name-text">{userName}</span>
-                            <span className="d-none d-xl-block ms-1 fs-12 text-muted user-name-sub-text">Founder</span>
+                            <span className="d-none d-xl-inline-block ms-1 fw-medium user-name-text">{user?.nama}</span>
+                            <span className="d-none d-xl-block ms-1 fs-12 text-muted user-name-sub-text">{user?.role}</span>
                         </span>
                     </span>
                 </DropdownToggle>
